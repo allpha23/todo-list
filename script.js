745040b18abb8304bc901eda5012bbf522e13e41
allpha23 <allpha23@gmail.com>
2022-04-06 18:32:37 -0300

    estilizando todo o projeto

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let btnApaga = document.querySelector('#apaga-tudo');
 let btnApagaCompletos = document.querySelector('#remover-finalizados');
 
 function criaTarefa() {
-  if (document.querySelector('#texto-tarefa').value == '') alert('digite uma tarefa');
+  if (document.querySelector('#texto-tarefa').value === '') alert('digite uma tarefa');
   else {
     let lista = document.querySelector('#lista-tarefas');
     let criaLista = document.createElement('li');
@@ -16,39 +16,39 @@ function criaTarefa() {
   selecionar();
 }
 
-function selecionar () {
+function selecionar() {
   let listaSelecionado = document.querySelectorAll('.lista');
-  for(let index = 0; index < listaSelecionado.length; index += 1){
+  for (let index = 0; index < listaSelecionado.length; index += 1) {
     listaSelecionado[index].addEventListener('click', selecionado);
     listaSelecionado[index].addEventListener('dblclick', tarefaFinalizada);
   }
 }
 
 function selecionado(origem) {
-  removeId = document.querySelector('#selecionado');
-  if(removeId !== null) removeId.removeAttribute('id');
+  let removeId = document.querySelector('#selecionado');
+  if (removeId !== null) removeId.removeAttribute('id');
   origem.target.id = 'selecionado';
 }
 
 function tarefaFinalizada(origem) {
-  if(origem.target.className == 'lista completed') origem.target.classList.remove('completed');
+  if (origem.target.className.includes('completed')) origem.target.classList.remove('completed');
   else origem.target.classList.add('completed');
 }
 
 function apagaLista() {
   let lista = document.querySelectorAll('.lista');
-  for(let index = lista.length - 1; index >= 0; index -= 1){
+  for (let index = lista.length - 1; index >= 0; index -= 1) {
     lista[index].remove();
   }
 }
 
 function apagaCompletos() {
   let listaCompletos = document.querySelectorAll('.completed');
-  for(let index = listaCompletos.length - 1; index >= 0; index -= 1){
+  for (let index = listaCompletos.length - 1; index >= 0; index -= 1) {
     listaCompletos[index].remove();
   }
 }
 
 btn.addEventListener('click', criaTarefa);
 btnApaga.addEventListener('click', apagaLista);
-btnApagaCompletos.addEventListener('click', apagaCompletos)
+btnApagaCompletos.addEventListener('click', apagaCompletos);
